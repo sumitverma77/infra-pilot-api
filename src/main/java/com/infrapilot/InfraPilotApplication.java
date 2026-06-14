fb773b16010b86446efb9a264a74466d2a58d2cb
sumitverma77 <987sumitverma@gmail.com>
2026-06-14 16:53:59 +0530

    add: Initialize application

---
+++ src/main/java/com/infrapilot/InfraPilotApplication.java
@@ -0,0 +1,13 @@
+package com.infrapilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraPilotApplication.class, args);
+    }
+
+}
